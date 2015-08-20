2da5edfded01a062f0256d2073948f76880ad7bd
MKS-students <mks.students@hackreactor.com>
2015-08-19 19:55:17 -0500

    completed helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -11,7 +12,7 @@ var _ = require('underscore');
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
-  archivedSites: path.join(__dirname, '../archives/sites'),
+  archivedSites: path.join(__dirname, '../archives/sites/'),
   list: path.join(__dirname, '../archives/sites.txt')
 };
 
@@ -25,17 +26,73 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(callback){
+  //fs.readfile
+  //read the sites.txt file
+  var urls ='';
+  fs.readFile(exports.paths.list, function (err, data) {
+    if (err) {
+      throw err;
+    }
+
+    if(callback){
+      urls += data
+      callback(urls.split('\n'));
+       }
+
+return urls;
+
+});
+
+
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(url, callback){
+  var string = '';
+  fs.readFile(exports.paths.list, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    if(callback){
+      string += data;
+      callback(string.indexOf(url) > -1)
+
+     }
+  })
+
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(url, callback){
+
+  fs.appendFile(exports.paths.list, url + '\n', function(err, data){
+    if(err){
+      throw err;
+    }
+
+      callback()
+
+  })
+
+
 };
 
-exports.isUrlArchived = function(){
+exports.isUrlArchived = function(url, callback){
+
+  var justinsway = path.join(exports.paths.archivedSites, url);
+  fs.exists(justinsway, function (exists) {
+
+    callback(exists);
+  })
 };
 
-exports.downloadUrls = function(){
+exports.downloadUrls = function(urlArray){
+//url = String(url);
+var html = '<html>reagan finally</html>'
+
+  urlArray.forEach(function(url){
+   // console.log('isUrlArchived:'+exports.isUrlArchived(url))
+   // if(!exports.isUrlArchived(url)){
+      fs.writeFile(exports.paths.archivedSites + url, html);
+   // }
+  });
 };
